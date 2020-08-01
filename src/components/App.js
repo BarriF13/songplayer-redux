402459d94165cd72e931ec98021fd1cc6584bf74
Barri <info@bdesignstudio.co.uk>
2020-08-01 10:53:13 +0100

    init project

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+//less use of class component cause we are using redux so no state needed 
+const App = () => {
+  return (
+    <div>
+      ola---moa app 
+    </div>
+  )
+}
+
+export default App
